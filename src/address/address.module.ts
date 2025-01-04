caa119ae2230bdba422d7312d8d053df074cba68
xandedj <c.alexandre2000@gmail.com>
2025-01-04 16:26:09 -0300

    feat: insert controller address

--- src/address/address.module.ts
+++ src/address/address.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AddressController } from './address.controller';
+import { AddressService } from './address.service';
+import { AddressEntity } from './entities/address.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  controllers: [AddressController]
+  imports: [TypeOrmModule.forFeature([AddressEntity])],
+  controllers: [AddressController],
+  providers: [AddressService],
 })
 export class AddressModule {}
---
+++ src/address/dtos/returnUser.dto.ts
@@ -0,0 +1,19 @@
+// import { IsNumber, IsString } from 'class-validator';
+// import { UserEntity } from '../entities/user.entity';
+
+// export class ReturnUserDto {
+//   id: number;
+//   name: string;
+//   email: string;
+//   phone: string;
+//   cpf: string;
+//   password: string;
+
+//   constructor(userEntity: UserEntity) {
+//     this.id = userEntity.id;
+//     this.name = userEntity.name;
+//     this.email = userEntity.email;
+//     this.phone = userEntity.phone;
+//     this.cpf = userEntity.cpf;
+//   }
+// }
